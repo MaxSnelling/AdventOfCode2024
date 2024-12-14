d9ac38ca9c582c3dbbc16de4f6ca64bd7a6521a4
Max Snelling <max.snelling@outlook.com>
2024-12-14 18:55:50 +0000

    Day 14 Solution

---
+++ src/Day14/Robot.java
@@ -0,0 +1,15 @@
+package Day14;
+
+public class Robot {
+    public int xPos;
+    public int yPos;
+    public int xVelocity;
+    public int yVelocity;
+
+    public Robot(int xPos, int yPos, int xVelocity, int yVelocity) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.xVelocity = xVelocity;
+        this.yVelocity = yVelocity;
+    }
+}
